5d598cfb7dfddcdb49a544cc106b6f6ebca039d5
plaza12 <baezlucas@gmail.com>
2023-04-07 16:15:10 -0300

    esta es mi primera calculadora

---
+++ script.js
@@ -0,0 +1,11 @@
+let display = document.getElementById('display');
+function addToDisplay(value) {
+  display.value += value;
+}
+function clearDisplay() {
+  display.value = '';
+}
+function calculate() {
+  let result = eval(display.value);
+  display.value = result;
+}
